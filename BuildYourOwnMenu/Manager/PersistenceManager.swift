5e849762386a9bdbb838b1b3f24c6502c14b15e9
bsrirangaswamy <30527688+bsrirangaswamy@users.noreply.github.com>
2019-05-26 20:57:06 -0400

    Unit Tests
    => Added unit tests for preload and data update for the main menu group.

--- BuildYourOwnMenu/Manager/PersistenceManager.swift
+++ BuildYourOwnMenu/Manager/PersistenceManager.swift
@@ -13,13 +13,23 @@ class PersistenceManager: NSObject {
     
     static let sharedInstance = PersistenceManager()
     
-    var managedContext: NSManagedObjectContext? = nil
+    // MARK: - Core Data stack
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "BuildYourOwnMenu")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
     
     lazy var fetchedResultsController: NSFetchedResultsController<MenuGroup> = {
-        let fetchRequest: NSFetchRequest<MenuGroup> = MenuGroup.fetchRequest()
+        let managedContext: NSManagedObjectContext = PersistenceManager.sharedInstance.persistentContainer.viewContext
+        let fetchRequest = NSFetchRequest<MenuGroup>(entityName: "MenuGroup")
         let sortDescriptor = NSSortDescriptor(key: "name", ascending: false)
         fetchRequest.sortDescriptors = [sortDescriptor]
-        let fetchRequestContrller = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: self.managedContext!, sectionNameKeyPath: nil, cacheName: nil)
+        let fetchRequestContrller = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: managedContext, sectionNameKeyPath: nil, cacheName: nil)
         do {
             try fetchRequestContrller.performFetch()
             print("Bala fetch successful")
@@ -32,12 +42,14 @@ class PersistenceManager: NSObject {
     // MARK :- Add, Update and Delete functions for Main menu group Items
     
     func addMainMenuData(name: String, imageData: Data?) {
-        let menuGroupAdd = MenuGroup(context: managedContext!)
-        menuGroupAdd.name = name
-        menuGroupAdd.imageData = imageData
-        menuGroupAdd.lastUpdated = Date()
+        let managedContext: NSManagedObjectContext = PersistenceManager.sharedInstance.persistentContainer.viewContext
+        let menuGroupEntity = NSEntityDescription.entity(forEntityName: "MenuGroup", in: managedContext)!
+        let menuGroupAdd = NSManagedObject(entity: menuGroupEntity, insertInto: managedContext)
+        menuGroupAdd.setValue(name, forKey: "name")
+        menuGroupAdd.setValue(imageData, forKey: "imageData")
+        menuGroupAdd.setValue(Date(), forKey: "lastUpdated")
         do {
-            try managedContext!.save()
+            try managedContext.save()
             print("Bala save successful")
         } catch let error as NSError {
             print("Save failed: error = \(error) and description = \(error.userInfo)")
@@ -45,12 +57,13 @@ class PersistenceManager: NSObject {
     }
     
     func updateMainMenuData(indexPath: IndexPath, name: String, imageData: Data?) {
+        let managedContext: NSManagedObjectContext = PersistenceManager.sharedInstance.persistentContainer.viewContext
         let menuGroupUpdate = self.fetchedResultsController.object(at: indexPath)
         menuGroupUpdate.name = name
         menuGroupUpdate.imageData = imageData
         menuGroupUpdate.lastUpdated = Date()
         do {
-            try managedContext?.save()
+            try managedContext.save()
             print("Bala update saved successful")
         } catch let error as NSError {
             print("Update Save failed: error = \(error) and description = \(error.userInfo)")
@@ -58,10 +71,11 @@ class PersistenceManager: NSObject {
     }
     
     func deleteMainMenuData(indexPath: IndexPath) {
+        let managedContext: NSManagedObjectContext = PersistenceManager.sharedInstance.persistentContainer.viewContext
         let menuGroupDelete = self.fetchedResultsController.object(at: indexPath)
-        self.managedContext?.delete(menuGroupDelete)
+        managedContext.delete(menuGroupDelete)
         do {
-            try managedContext?.save()
+            try managedContext.save()
             print("Bala update saved successful")
         } catch let error as NSError {
             print("Update Save failed: error = \(error) and description = \(error.userInfo)")
@@ -71,15 +85,16 @@ class PersistenceManager: NSObject {
     // MARK :- Add, Update and Delete functions for sub menu group Items
     
     func addSubMenuDataInMainMenu(atIndexPath: IndexPath, name: String, price: String?, imageData: Data?) {
+        let managedContext: NSManagedObjectContext = PersistenceManager.sharedInstance.persistentContainer.viewContext
         let menuGroupUpdate = self.fetchedResultsController.object(at: atIndexPath)
-        let subMenuAdd = SubMenuItem(context: managedContext!)
+        let subMenuAdd = SubMenuItem(context: managedContext)
         subMenuAdd.itemName = name
         subMenuAdd.itemPrice = price
         subMenuAdd.itemImageData = imageData
         menuGroupUpdate.lastUpdated = Date()
         menuGroupUpdate.addToSubMenuItem(subMenuAdd)
         do {
-            try managedContext!.save()
+            try managedContext.save()
             print("Bala save successful")
         } catch let error as NSError {
             print("Save failed: error = \(error) and description = \(error.userInfo)")
@@ -87,6 +102,7 @@ class PersistenceManager: NSObject {
     }
     
     func updateSubMenuDataInMainMenu(atIndexPath: IndexPath, subMenuIndex: Int, name: String, price: String?, imageData: Data?) {
+        let managedContext: NSManagedObjectContext = PersistenceManager.sharedInstance.persistentContainer.viewContext
         let menuGroupUpdate = self.fetchedResultsController.object(at: atIndexPath)
         guard let subMenuUpdate = menuGroupUpdate.subMenuItem?[subMenuIndex] as? SubMenuItem else {
             return
@@ -96,7 +112,7 @@ class PersistenceManager: NSObject {
         subMenuUpdate.itemImageData = imageData
         menuGroupUpdate.lastUpdated = Date()
         do {
-            try managedContext!.save()
+            try managedContext.save()
             print("Bala Update save successful")
         } catch let error as NSError {
             print("Update Save failed: error = \(error) and description = \(error.userInfo)")
@@ -104,10 +120,11 @@ class PersistenceManager: NSObject {
     }
     
     func deleteSubMenuDataInMainMenu(atIndexPath: IndexPath, subMenuIndex: Int) {
+        let managedContext: NSManagedObjectContext = PersistenceManager.sharedInstance.persistentContainer.viewContext
         let menuGroupUpdate = self.fetchedResultsController.object(at: atIndexPath)
         menuGroupUpdate.removeFromSubMenuItem(at: subMenuIndex)
         do {
-            try managedContext!.save()
+            try managedContext.save()
             print("Bala delet save successful")
         } catch let error as NSError {
             print("Delete Save failed: error = \(error) and description = \(error.userInfo)")
@@ -115,6 +132,7 @@ class PersistenceManager: NSObject {
     }
     
     func preLoadData() {
+        let managedContext: NSManagedObjectContext = PersistenceManager.sharedInstance.persistentContainer.viewContext
         let menuGroup1 = createMenuGroup(name: "Waffles", imageName: "waffles")
         menuGroup1.addToSubMenuItem(createSubItem(name: "Chocolate Waffle", imageName: "chocolateWaffle", price: "7.99$"))
         menuGroup1.addToSubMenuItem(createSubItem(name: "Banana Waffle", imageName: "bananaWaffle", price: "8.99$"))
@@ -130,7 +148,7 @@ class PersistenceManager: NSObject {
         menuGroup3.addToSubMenuItem(createSubItem(name: "Blueberry Bagel", imageName: "blueberryBagel", price: "6.99$"))
         menuGroup3.addToSubMenuItem(createSubItem(name: "Everything Bagel", imageName: "everythingBagel", price: "7.99$"))
         do {
-            try managedContext!.save()
+            try managedContext.save()
             print("Bala save successful")
         } catch let error as NSError {
             print("Save failed: error = \(error) and description = \(error.userInfo)")
@@ -138,7 +156,8 @@ class PersistenceManager: NSObject {
     }
     
     private func createMenuGroup(name: String?, imageName: String) -> MenuGroup {
-        let menuGroupCreated = MenuGroup(context: managedContext!)
+        let managedContext: NSManagedObjectContext = PersistenceManager.sharedInstance.persistentContainer.viewContext
+        let menuGroupCreated = MenuGroup(context: managedContext)
         menuGroupCreated.name = name
         menuGroupCreated.imageData = UIImage(named: imageName)?.jpegData(compressionQuality: 1)
         menuGroupCreated.lastUpdated = Date()
@@ -146,10 +165,25 @@ class PersistenceManager: NSObject {
     }
     
     private func createSubItem(name: String?, imageName: String, price: String?) -> SubMenuItem {
-        let subMenuItemCreated = SubMenuItem(context: managedContext!)
+        let managedContext: NSManagedObjectContext = PersistenceManager.sharedInstance.persistentContainer.viewContext
+        let subMenuItemCreated = SubMenuItem(context: managedContext)
         subMenuItemCreated.itemName = name
         subMenuItemCreated.itemPrice = price
         subMenuItemCreated.itemImageData = UIImage(named: imageName)?.jpegData(compressionQuality: 1)
         return subMenuItemCreated
+    }
+    
+    // MARK: - Core Data Saving support
+    
+    func saveContext () {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
     }
 }
--- BuildYourOwnMenuTests/BuildYourOwnMenuTests.swift
+++ BuildYourOwnMenuTests/BuildYourOwnMenuTests.swift
@@ -10,25 +10,32 @@ import XCTest
 @testable import BuildYourOwnMenu
 
 class BuildYourOwnMenuTests: XCTestCase {
+    
+    var pMTest: PersistenceManager!
 
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        super.setUp()
+        pMTest = PersistenceManager.sharedInstance
     }
 
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
     }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func test_data_PreLoad() {
+        let coreDataObjectCount = pMTest.fetchedResultsController.sections?[0].numberOfObjects ?? 0
+        XCTAssertEqual(coreDataObjectCount, 3, "PreLoad to core data failed")
     }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func test_update_Data() {
+        let coreDataObjectCount = pMTest.fetchedResultsController.sections?[0].numberOfObjects ?? 0
+        let indPath = IndexPath(row: coreDataObjectCount-1, section: 0)
+        let menuUpdateObject = pMTest.fetchedResultsController.object(at: indPath)
+        let nameBefore = menuUpdateObject.name!
+        pMTest.updateMainMenuData(indexPath: indPath, name: nameBefore+nameBefore, imageData: nil)
+        let menuUpdateObjectAfter = pMTest.fetchedResultsController.object(at: indPath)
+        let nameAfter = menuUpdateObjectAfter.name!
+        XCTAssertEqual(nameAfter, nameBefore+nameBefore, "Update to core data failed")
     }
 
 }
